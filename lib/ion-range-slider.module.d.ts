08261f0afbd196b8a5ef3d9f9cccc4caca7273de
Yevhen Kotliar <eugen.kotliar@gmail.com>
2020-03-14 21:47:38 -0400

    dependecies

--- lib/ion-range-slider.module.d.ts
+++ lib/ion-range-slider.module.d.ts
@@ -1,2 +1,6 @@
+import * as i0 from "@angular/core";
+import * as i1 from "./ion-range-slider.component";
 export declare class IonRangeSliderModule {
+    static ɵmod: i0.ɵɵNgModuleDefWithMeta<IonRangeSliderModule, [typeof i1.IonRangeSliderComponent], never, [typeof i1.IonRangeSliderComponent]>;
+    static ɵinj: i0.ɵɵInjectorDef<IonRangeSliderModule>;
 }
